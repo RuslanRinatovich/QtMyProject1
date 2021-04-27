df255d9c31ad8fe5f50c08a0187ee583a7763de3
ruslan <ruslan@ruslan.ru>
2021-04-27 08:49:38 +0300

    222

---
+++ yandexmap.py
@@ -0,0 +1,45 @@
+import requests
+
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
+
+
+def geocode(address):
+    # Собираем запрос для геокодера.
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": API_KEY,
+        "geocode": address,
+        "format": "json"}
+
+    # Выполняем запрос.
+    response = requests.get(geocoder_request, params=geocoder_params)
+
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+    else:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code} ({response.reason})""")
+
+    # Получаем первый топоним из ответа геокодера.
+    # Согласно описанию ответа он находится по следующему пути:
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
+# Получаем координаты объекта по его адресу.
+def get_coordinates(address):
+    toponym = geocode(address)
+    if not toponym:
+        return None, None
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Широта, преобразованная в плавающее число:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
+
+
+
